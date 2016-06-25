5f01702b06d54eba19aacbac641f035e3229fc75
Scott Shawcroft <scott@chickadee.tech>
2016-06-25 10:17:20 -0700

    Add support for configuring the SD card detect pin.

--- src/main/drivers/sdcard.c
+++ src/main/drivers/sdcard.c
@@ -24,6 +24,46 @@
 #include "nvic.h"
 #include "io.h"
 
+// Begin headers to access masterConfig.
+#include "common/axis.h"
+#include "common/maths.h"
+
+#include "sensors/sensors.h" // before sensor.h
+#include "drivers/sensor.h" // before accgyro!
+#include "drivers/accgyro.h"
+
+#include "drivers/gpio.h" // before timer!
+#include "drivers/timer.h" // before pwm_rx!
+#include "drivers/pwm_rx.h"
+#include "drivers/serial.h"
+#include "drivers/sound_beeper.h"
+
+#include "sensors/acceleration.h"
+#include "flight/failsafe.h"
+#include "flight/pid.h" // before imu.!
+#include "flight/imu.h"
+#include "flight/mixer.h"
+#include "flight/navigation.h"
+
+#include "io/escservo.h"
+#include "io/gimbal.h"
+#include "io/gps.h"
+#include "io/rc_controls.h"
+#include "io/serial.h"
+
+#include "sensors/barometer.h"
+#include "sensors/battery.h"
+#include "sensors/boardalignment.h"
+#include "sensors/gyro.h"
+
+#include "telemetry/telemetry.h"
+
+#include "config/runtime_config.h"
+#include "config/config.h"
+#include "config/config_profile.h"
+// End headers to access masterConfig.
+#include "config/config_master.h"
+
 #include "drivers/bus_spi.h"
 #include "drivers/system.h"
 
@@ -125,7 +165,7 @@ static IO_t sdCardCsPin = IO_NONE;
 void sdcardInsertionDetectDeinit(void)
 {
 #ifdef SDCARD_DETECT_PIN
-    sdCardDetectPin = IOGetByTag(IO_TAG(SDCARD_DETECT_PIN));
+    sdCardDetectPin = IOGetByTag(masterConfig.sdcard_detect_pin);
     IOInit(sdCardDetectPin, OWNER_SYSTEM, RESOURCE_SPI);
     IOConfigGPIO(sdCardDetectPin, IOCFG_IN_FLOATING);
 #endif
@@ -134,7 +174,7 @@ void sdcardInsertionDetectDeinit(void)
 void sdcardInsertionDetectInit(void)
 {
 #ifdef SDCARD_DETECT_PIN
-    sdCardDetectPin = IOGetByTag(IO_TAG(SDCARD_DETECT_PIN));
+    sdCardDetectPin = IOGetByTag(masterConfig.sdcard_detect_pin);
     IOInit(sdCardDetectPin, OWNER_SDCARD, RESOURCE_INPUT);
     // TODO(tannewt): Shouldn't this pull down if the pin is not inverted?
     IOConfigGPIO(sdCardDetectPin, IOCFG_IPU);
